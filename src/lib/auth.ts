bad86759c33ead72b6db857247154f24eba24764
noilab8128 <noi.lab.8128@gmail.com>
2026-03-02 15:51:08 +0900

    login fuction err fixed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,6 +17,11 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: {
+                params: {
+                    prompt: "select_account"
+                }
+            }
         }),
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID as string,
